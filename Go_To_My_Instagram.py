66f8c08f76d0beea5d6818467ba32fdedef984d3
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-02 10:48:23 +0200

    Create Go_To_My_Instagram.py

---
+++ Go_To_My_Instagram.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# Path to chromedriver.exe
+path = r"H:\Code\GitHub_Repos\Buy_Bot\chromedriver.exe"
+
+# Initialize the browser
+browser = webdriver.Chrome(executable_path=path)
+
+# Startup page
+site = "https://www.google.com"
+browser.get(site)
+
+# Accept cookies
+browser.find_element_by_xpath('//*[@id="zV9nZe"]/div').click()
+
+# Select search bar
+searchbar = browser.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+
+# Search google
+search = "python_genius"
+
+searchbar.click()
+searchbar.send_keys(search)
+searchbar.send_keys("\n")
+
+# Click on Instagram link and accept cookies
+browser.find_element_by_xpath('//*[@id="rso"]/div[1]/div[1]/div/div[1]/a/h3').click()
+browser.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/button[1]').click()
+
